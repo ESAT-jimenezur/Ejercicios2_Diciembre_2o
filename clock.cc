f9c83c512de0711529cdfa2ce7e192f554eb11c5
ESAT-jimenezur <jimenezur@esat-alumni.com>
2014-12-10 18:48:53 +0100

    Adding clock classes

---
+++ clock.cc
@@ -0,0 +1,6 @@
+/* Clock.cc
+*  Simple implementation of a clock class
+*
+*  @author  Jose Luis Jimenez Urbano | <ijos@ijos.es> | < @iJos >
+*  December 2014
+*/
---
+++ clock.h
@@ -0,0 +1,6 @@
+/* Clock.h
+*  Simple declaration of a clock class
+*
+*  @author  Jose Luis Jimenez Urbano | <ijos@ijos.es> | < @iJos >
+*  December 2014
+*/
